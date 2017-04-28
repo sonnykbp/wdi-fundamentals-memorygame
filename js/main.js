6816b1c715e5b477b5de5b5a4eef924d1f2ddb9d
sonnykbp <SonnyKeobounphanh@sounthisays-MBP.csnetLan>
2017-04-27 22:01:40 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -2,22 +2,31 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
 
-var cardTwo = cards[1];
-cardsInPlay.push(cardTwo);
+function checkForMatch() {
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
 
-if (cardsInPlay.length === 2) {
+		}
 
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-	alert("You found a match!");
-
-	}
-
-	else {
+		else {
 		alert("Sorry, try again.")
-	}
+		}
 }
 
-console.log("User flipped " + cardsInPlay);
+
+var flipCard = function (cardId) {
+	console.log("User flipped " + cards[cardId]);
+
+	cardsInPlay.push(cards[cardId]);
+
+
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+
+	}
+
+}
+
+flipCard(0);
+flipCard(2);
